82ef2745dde5b608680071b497878c49b47a4c75
jvnlee <lhjun1028@naver.com>
2022-02-24 23:06:38 +0900

    ADD: 회원 서비스 코드 작성

---
+++ src/main/java/demo/springbootdemo/service/MemberService.java
@@ -0,0 +1,37 @@
+package demo.springbootdemo.service;
+
+import demo.springbootdemo.domain.Member;
+import demo.springbootdemo.repository.MemberRepository;
+import demo.springbootdemo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원 가입
+    public Long join(Member member) {
+        // 중복된 이름의 회원이 있으면 안됨
+        validateExistingName(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateExistingName(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원 이름입니다.");
+                });
+    }
+
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
