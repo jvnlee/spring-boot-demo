49123ac318b6083be08fe462af67c4392fd03ce3
jvnlee <lhjun1028@naver.com>
2022-03-01 15:13:22 +0900

    ADD: MemberController에 URL 추가 맵핑, 뷰 템플릿 추가 생성

---
+++ src/main/java/demo/springbootdemo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package demo.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
--- src/main/java/demo/springbootdemo/controller/MemberController.java
+++ src/main/java/demo/springbootdemo/controller/MemberController.java
@@ -1,8 +1,14 @@
 package demo.springbootdemo.controller;
 
+import demo.springbootdemo.domain.Member;
 import demo.springbootdemo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /*
 컴포넌트 스캔으로 스프링 빈 등록하기
@@ -24,5 +30,28 @@ public class MemberController {
         컨트롤러인 MemberController의 객체 생성 시점에, 생성에 필요한 MemberService의 객체를 스프링 컨테이너에서 찾아서 주입시켜줌
         스프링 빈은 스프링 컨테이너 내에 싱글톤(유일한 객체)으로 등록되기 때문에 같은 스프링 빈이면 같은 인스턴스라고 볼 수 있음
          */
+    }
+
+    @GetMapping("/members/new")
+    public String createMemberForm() {
+        return "members/createMemberForm";
+    }
+
+    // 맵핑할 URL이 같아도 GET이냐 POST냐에 따라 다른 로직을 적용할 수 있음
+    @PostMapping("/members/new")
+    public String createMember(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        List<Member> members = memberService.findAllMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
